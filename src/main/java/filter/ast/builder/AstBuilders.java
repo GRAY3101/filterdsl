6b9630abe1997e642072878df5d5364f22dad7aa
GRAY3101 <adrianscheling.git@gmail.com>
2026-06-18 18:53:19 +0200

    DONE: AstBuilders

--- src/main/java/filter/ast/builder/AstBuilders.java
+++ src/main/java/filter/ast/builder/AstBuilders.java
@@ -12,10 +12,16 @@ public class AstBuilders {
   public static Expr fromQuery(String query, Function<FilterParser.QueryContext, Expr> translator) {
     return simplify(translator.apply(parse(query)));
   }
-
+    
   public static Expr simplify(Expr e) {
-    // TODO
-    return e;
+    return switch (e) {
+      case Expr.Not(Expr.Not(var inner)) -> simplify(inner); // Doppelte Negation eliminieren
+      case Expr.And(var l, var r) -> new Expr.And(simplify(l), simplify(r)); // And-Ausdrücke vereinfachen
+      case Expr.Or(var l, var r) -> new Expr.Or(simplify(l), simplify(r)); // Or-Ausdrücke vereinfachen
+      case Expr.Not(var inner) -> new Expr.Not(simplify(inner)); // Not-Ausdrücke vereinfachen
+      case Expr.Comparison(var field, var op, var value) -> e; // Vergleichsausdrücke bleiben unverändert
+      case Expr.InList(var field, var values) -> e; // In-List-Ausdrücke bleiben unverändert
+    };
   }
 
   public static FilterParser.QueryContext parse(String query) {
